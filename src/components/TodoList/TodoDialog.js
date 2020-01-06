30f571ed481c566f8a5b95eb29f7927e313d6058
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-01-06 21:39:20 +0100

    add backdrop

--- src/components/TodoList/TodoDialog.js
+++ src/components/TodoList/TodoDialog.js
@@ -1,47 +1,47 @@
 import React from "react";
 import {
-    Button,
-    Dialog,
-    DialogActions,
-    DialogContent,
-    Slide,
-    DialogTitle,
-    DialogContentText
+  Button,
+  Dialog,
+  DialogActions,
+  DialogContent,
+  Slide,
+  DialogTitle,
+  DialogContentText
 } from "@material-ui/core";
 const Transition = React.forwardRef(function Transition(props, ref) {
-    return <Slide direction="up" ref={ref} {...props} />;
+  return <Slide direction="up" ref={ref} {...props} />;
 });
 export default function TodoDialog({
-    approvedRemoveList,
-    openDialog,
-    dialogHandler,
-    list
+  approvedRemoveList,
+  dialog,
+  dialogHandler,
+  list
 }) {
-    return (
-        <Dialog
-            open={openDialog}
-            TransitionComponent={Transition}
-            keepMounted
-            onClose={dialogHandler}
-            aria-labelledby="alert-dialog-slide-title"
-            aria-describedby="alert-dialog-slide-description"
-        >
-            <DialogTitle id="alert-dialog-slide-title">
-                Do u want to remove {list ? list.title : null}?
-            </DialogTitle>
-            <DialogContent>
-                <DialogContentText id="alert-dialog-slide-description">
-                    this list will be deleted permanently
-                </DialogContentText>
-            </DialogContent>
-            <DialogActions>
-                <Button onClick={dialogHandler} color="primary">
-                    Disagree
-                </Button>
-                <Button onClick={approvedRemoveList} color="primary">
-                    Agree
-                </Button>
-            </DialogActions>
-        </Dialog>
-    );
+  return (
+    <Dialog
+      open={dialog}
+      TransitionComponent={Transition}
+      keepMounted
+      onClose={dialogHandler}
+      aria-labelledby="alert-dialog-slide-title"
+      aria-describedby="alert-dialog-slide-description"
+    >
+      <DialogTitle id="alert-dialog-slide-title">
+        Do u want to remove {list ? list.title : null}?
+      </DialogTitle>
+      <DialogContent>
+        <DialogContentText id="alert-dialog-slide-description">
+          this list will be deleted permanently
+        </DialogContentText>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={dialogHandler} color="primary">
+          Disagree
+        </Button>
+        <Button onClick={approvedRemoveList} color="primary">
+          Agree
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
 }
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -2,104 +2,118 @@ import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import { isEmpty } from "lodash";
 import TodoDnd from "./TodoDnd";
-import { Button, Typography, LinearProgress } from "@material-ui/core";
+import {
+  Button,
+  Typography,
+  LinearProgress,
+  Backdrop
+} from "@material-ui/core";
 import { request } from "../../api/API";
 import Cookie from "js-cookie";
 import Modal from "../Utils/Modal";
 import TodoCreateList from "./TodoCreateList";
 import TodoDialog from "./TodoDialog";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { makeStyles } from "@material-ui/core/styles";
+const useStyles = makeStyles(theme => ({
+  backdrop: {
+    zIndex: theme.zIndex.drawer + 1,
+    color: "#fff"
+  }
+}));
 export default function TodoList() {
-    const userID = useSelector(state => state.authReducer.data._id);
-    const [loading, setLoading] = useState(true);
-    const [todoList, setTodoList] = useState([]);
-    const [open, setOpen] = useState(false);
-    const [list, setList] = useState();
-    const [openDialog, setOpenDialog] = useState(false);
-
-    useEffect(() => {
-        request(
-            `${process.env.REACT_APP_SERVER}/api/todo/get-user-todo-lists`,
-            { userID },
-            Cookie.get("token")
-        )
-            .then(res => {
-                setLoading(false);
-                setTodoList(res.data);
-            })
-            .catch(error => {
-                console.log(error.response);
-            });
-    }, [userID]);
-    const getListHandler = () => {
-        request(
-            `${process.env.REACT_APP_SERVER}/api/todo/get-user-todo-lists`,
-            { userID },
-            Cookie.get("token")
-        )
-            .then(res => {
-                setLoading(false);
-                setTodoList(res.data);
-            })
-            .catch(error => {
-                console.log(error.response);
-            });
-    };
-    const removeListHandler = list => {
-        setList(list);
-        setOpenDialog(true);
-    };
-    const approvedRemoveList = () => {
-        request(
-            `${process.env.REACT_APP_SERVER}/api/todo/remove-todo-list`,
-            { listID: list._id },
-            Cookie.get("token"),
-            "delete"
-        )
-            .then(res => {
-                return res.data;
-            })
-            .catch(error => {
-                console.log(error.response);
-            });
-        getListHandler();
-        setOpenDialog(false);
-    };
-    const modalHandler = () => {
-        setOpen(!open);
-    };
-    const dialogHandler = () => {
-        setOpenDialog(!openDialog);
-    };
-    return (
-        <>
-            <Button variant="contained" color="primary" onClick={modalHandler}>
-                Create new Todo List
-            </Button>
-            {loading ? (
-                <LinearProgress style={{ margin: "10px 0" }} />
-            ) : isEmpty(todoList) ? (
-                <Typography variant="subtitle1" style={{ margin: "10px 0" }}>
-                    You do not have any list currently
-                </Typography>
-            ) : (
-                <TodoDnd
-                    todoList={todoList}
-                    removeListHandler={removeListHandler}
-                />
-            )}
-            <Modal modalHandler={modalHandler} openProps={open} width={800}>
-                <TodoCreateList
-                    modalHandler={modalHandler}
-                    user={userID}
-                    getListHandler={getListHandler}
-                />
-            </Modal>
-            <TodoDialog
-                approvedRemoveList={approvedRemoveList}
-                openDialog={openDialog}
-                dialogHandler={dialogHandler}
-                list={list}
-            />
-        </>
-    );
+  const classes = useStyles();
+  const userID = useSelector(state => state.authReducer.data._id);
+  const [loading, setLoading] = useState(true);
+  const [todoList, setTodoList] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [list, setList] = useState();
+  const [dialog, setDialog] = useState(false);
+  const [backdrop, setBackdrop] = useState(false);
+  useEffect(() => {
+    request(
+      `${process.env.REACT_APP_SERVER}/api/todo/get-user-todo-lists`,
+      { userID },
+      Cookie.get("token")
+    )
+      .then(res => {
+        setLoading(false);
+        setTodoList(res.data);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  }, [userID]);
+  const getListHandler = () => {
+    setBackdrop(true);
+    request(
+      `${process.env.REACT_APP_SERVER}/api/todo/get-user-todo-lists`,
+      { userID },
+      Cookie.get("token")
+    )
+      .then(res => {
+        setLoading(false);
+        setTodoList(res.data);
+        setBackdrop(false);
+      })
+      .catch(error => {
+        console.log(error.response);
+      });
+  };
+  const removeListHandler = list => {
+    setList(list);
+    setDialog(true);
+  };
+  const approvedRemoveList = () => {
+    request(
+      `${process.env.REACT_APP_SERVER}/api/todo/remove-todo-list`,
+      { listID: list._id },
+      Cookie.get("token"),
+      "delete"
+    )
+      .then(() => {})
+      .catch(error => {
+        console.log(error.response);
+      });
+    getListHandler();
+    setDialog(false);
+  };
+  const modalHandler = () => {
+    setOpen(!open);
+  };
+  const dialogHandler = () => {
+    setDialog(!dialog);
+  };
+  return (
+    <>
+      <Button variant="contained" color="primary" onClick={modalHandler}>
+        Create new Todo List
+      </Button>
+      {loading ? (
+        <LinearProgress style={{ margin: "10px 0" }} />
+      ) : isEmpty(todoList) ? (
+        <Typography variant="subtitle1" style={{ margin: "10px 0" }}>
+          You do not have any list currently
+        </Typography>
+      ) : (
+        <TodoDnd todoList={todoList} removeListHandler={removeListHandler} />
+      )}
+      <Modal modalHandler={modalHandler} openProps={open} width={800}>
+        <TodoCreateList
+          modalHandler={modalHandler}
+          user={userID}
+          getListHandler={getListHandler}
+        />
+      </Modal>
+      <TodoDialog
+        approvedRemoveList={approvedRemoveList}
+        dialog={dialog}
+        dialogHandler={dialogHandler}
+        list={list}
+      />
+      <Backdrop className={classes.backdrop} open={backdrop}>
+        <CircularProgress color="inherit" />
+      </Backdrop>
+    </>
+  );
 }
