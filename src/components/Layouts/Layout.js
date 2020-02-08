9855e9aa4f7067432cc81164a93ccd020bfa9680
Maciej Bednarczyk <m.bednarczyk.1991@gmail.com>
2020-02-08 14:02:14 +0100

    add google analytics

--- src/components/Layouts/Layout.js
+++ src/components/Layouts/Layout.js
@@ -1,11 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
 import Drawer from "./Drawer";
 import AppBar from "./AppBar";
+import { withRouter } from "react-router-dom";
+import ReactGA from "react-ga";
 
-export default function Layout({ isAuth, children }) {
+function initializeReactGA() {
+  ReactGA.initialize(process.env.REACT_APP_TRACKING_ID, {
+    gaOptions: {
+      siteSpeedSampleRate: 100
+    }
+  });
+  ReactGA.pageview(window.location.pathname + window.location.search);
+}
+export default withRouter(function Layout({ isAuth, children, location }) {
+  const [currentPath, setCurrentPath] = useState(location.pathname);
+
+  useEffect(() => {
+    const { pathname } = location;
+
+    setCurrentPath(pathname);
+    initializeReactGA(currentPath);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [location.pathname]);
+
   if (isAuth) {
     return <Drawer>{children}</Drawer>;
   } else {
     return <AppBar>{children}</AppBar>;
   }
-}
+});
